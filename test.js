88b3380dbb6d5fe9ce63bc6f2246e0fc20bd9f5b
junghunchoi <azzudi@naver.com>
2023-12-13 18:08:19 +0900

    [2023.12.13] 회사업무 정리

--- test.js
+++ test.js
@@ -1,6 +1,22 @@
-const obj = {
-  name: 'test',
-  age: 30,
+let i = 0;
+
+let start = Date.now();
+
+function count() {
+
+  // 스케줄링 코드를 함수 앞부분으로 옮김
+  if (i < 1e9 - 1e6) {
+    setTimeout(count); // 새로운 호출을 스케줄링함
+  }
+
+  do {
+    i++;
+  } while (i % 1e6 != 0);
+
+  if (i == 1e9) {
+    console.log("처리에 걸린 시간: " + (Date.now() - start) + "ms");
+  }
+
 }
 
-console.log(obj.)
+count();
