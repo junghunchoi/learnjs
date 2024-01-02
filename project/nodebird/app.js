8b2a2c365b54b3bf48ade8c21f8d3e275d5b3027
junghunchoi <azzudi@naver.com>
2024-01-02 18:08:53 +0900

    [2023.01.02] 노드교과서 오늘 하나 끝내기 화이팅 !

---
+++ project/nodebird/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const nunjucks = require('nunjucks');
+
+dotenv.config();
+const pageRouter = require('./routes/page');
+
+const app = express();
+app.set('port', process.env.PORT || 8001);
+
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET, // .env 파일에 COOKIE_SECRET=secret 입력
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  }
+}));
+
+app.use('/', pageRouter);
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500)
+  res.render('error');
+});
